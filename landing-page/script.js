444df8c6f7107c2e4ba3922f1d4a53f326db04b3
Luis <luisftomasprado@gmail.com>
2024-07-31 19:21:43 -0300

    initial commit

--- landing-page/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("theme-toggle").addEventListener("click", function () {
-	const bodyElement = document.body;
-
-	if (bodyElement.classList.contains("dark-mode")) {
-		bodyElement.classList.remove("dark-mode");
-	} else {
-		bodyElement.classList.add("dark-mode");
-	}
-});
